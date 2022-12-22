f2b5f23edc0e79b37d4d14e712389bbb9ff84fe2
SauronLee <lixira@outlook.com>
2022-12-22 14:13:44 +0900

    20221222

--- source/conf.py
+++ source/conf.py
@@ -54,16 +54,19 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_theme
-html_theme = 'stanford_theme'
-html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
+#import sphinx_theme
+#html_theme = 'stanford_theme'
+#html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+import sphinx_rtd_theme
 extensions = [
     'recommonmark',
+    'sphinx_rtd_theme',
     'sphinx_markdown_tables'
 ]
+
+html_theme = "sphinx_rtd_theme"
